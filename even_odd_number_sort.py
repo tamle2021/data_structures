85652b038d7fae7d3c925bc86cec6b4e8f189f9b
tamle2021 <tamle2021@zoho.com>
2021-04-30 07:09:14 -0500

    Cleanup even odd number sort

--- even_odd_number_sort.py
+++ even_odd_number_sort.py
@@ -2,19 +2,24 @@
 Sort array and also return swap count
 '''
 
+count = 0
+
 def sort(arr):
-    index = 0;
+    global count
+    count = 0
+    index = 0
     n = len(arr)
     while (index < int(n)):
         if (index == 0):
             index = index + 1
         if (compare(arr[index],arr[index - 1])):
-            index = index + 1;
+            index = index + 1
         else:
-            temp = arr[index];
-            arr[index] = arr[index - 1];
-            arr[index - 1] = temp;
+            temp = arr[index]
+            arr[index] = arr[index - 1]
+            arr[index - 1] = temp
             index = index - 1
+            count = count + 1
     return arr
 
 def compare(a,b) :
@@ -24,6 +29,8 @@ def compare(a,b) :
         return a % 2 == 1
 
 if (__name__ == "__main__"):
-    a = [3, 2, 5, 6, 4, 9, 11,7]
-    array = sort(a)
-    print(array)
+    arr = [3,2,5,6,4,9,11,7]
+    print("before sort: %s" % arr)
+    array = sort(arr)
+    print("after sort: %s" % array)
+    print("count swap: {x}".format(x=count))
