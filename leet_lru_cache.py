453653d7322f3c38e8099e41bb2db6973e826abb
tamle2021 <tamle2021@zoho.com>
2021-05-10 10:03:49 -0500

    Update LRU cache

--- leet_lru_cache.py
+++ leet_lru_cache.py
@@ -1,20 +1,19 @@
 '''
 Least recently used policy to remove items from cache
 
-
 '''
 
 from collections import deque
 
 class LRUCache:
     def __init__(self,capacity: int):
-        self.c = capacity
-        self.m = dict()
+        self.capacity = capacity
+        self.map = dict()
         self.deq = deque()
 
-    def get(self, key: int) -> int:
-        if key in self.m:
-            value = self.m[key]
+    def get(self,key: int) -> int:
+        if (key in self.map):
+            value = self.map[key]
             self.deq.remove(key)
             self.deq.append(key)
             return value
@@ -22,25 +21,30 @@ class LRUCache:
             return -1
 
     def put(self,key: int,value: int) -> None:
-        # LRU cache object will be instantiated and called as such:
-        # obj = LRUCache(capacity)
+        # LRU cache object will be instantiated and called as such: obj = LRUCache(capacity)
         # param1 = obj.get(key)
         # obj.put(key,value)
-        if (key not in self.m):
-            if len(self.deq) == self.c:
+        if (key not in self.map):
+            if len(self.deq) == self.capacity:
                 oldest = self.deq.popleft()
-                del self.m[oldest]
+                del self.map[oldest]
         else:
             self.deq.remove(key)
 
-        self.m[key] = value
+        self.map[key] = value
         self.deq.append(key)
 
     def displayDeq(self):
         print(self.deq)
 
+    def displayMap(self):
+        print("map: {0}".format(self.map))
+
 if (__name__ == "__main__"):
     l = LRUCache(5)
     l.put(6,2)
     l.put(1,3)
+    l.put(4,4)
+    l.put(9,144)
     l.displayDeq()
+    l.displayMap()
