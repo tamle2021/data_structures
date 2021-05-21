a538ead90275c6c6d25cda0c28d3ed5ca399dec6
tamle2021 <tamle2021@zoho.com>
2021-05-21 09:53:31 -0500

    Update bubble sort

--- c++/bubble_sort.cpp
+++ c++/bubble_sort.cpp
@@ -1,4 +1,26 @@
 /*
+Bubble Sort is a sorting algorithm that works by repeatedly swapping the adjacent elements if they
+are in wrong order.
+example:
+first pass:
+( 5 1 4 2 8 ) --> ( 1 5 4 2 8 ) Here, algorithm compares the first two elements, and swaps since 5 > 1.
+( 1 5 4 2 8 ) -->  ( 1 4 5 2 8 ) Swap since 5 > 4
+( 1 4 5 2 8 ) -->  ( 1 4 2 5 8 ) Swap since 5 > 2
+( 1 4 2 5 8 ) --> ( 1 4 2 5 8 ) Now, since these elements are already in order (8 > 5), algorithm does not
+swap them.
+second pass:
+(1 4 2 5 8) --> (1 4 2 5 8)
+(1 4 2 5 8) --> (1 2 4 5 8) Swap since 4 > 2
+(1 2 4 5 8) --> (1 2 4 5 8)
+(1 2 4 5 8) -->  (1 2 4 5 8)
+Now, the array is already sorted, but algorithm does not know if it is completed. The algorithm needs
+one whole pass without any swap to know it is sorted.
+third pass:
+(1 2 4 5 8) --> (1 2 4 5 8)
+(1 2 4 5 8) --> (1 2 4 5 8)
+(1 2 4 5 8) --> (1 2 4 5 8)
+(1 2 4 5 8) --> (1 2 4 5 8)
+
 minimum time: O(n)
 max time: O(n^2)
 adaptive & stable
@@ -9,7 +31,7 @@ using namespace std;
 
 template <class T>
 void printArray(T& vec,int n,string s) {
-    cout << s << ": [" << flush;
+    cout << s << "[" << flush;
     for (int i = 0; i < n; i++) {
         cout << vec[i] << flush;
         if (i < n - 1) {
@@ -45,11 +67,11 @@ int main() {
     int n = sizeof(A) / sizeof(A[0]);
     cout << "*****bubble sort*****\n" << endl;
     cout << "---before sort---\n";
-    printArray(A,n,"array:");
+    printArray(A,n,"");
 
     bubbleSort(A,n);
     cout << "---after sort---\n";
-    printArray(A,n,"array: ");
+    printArray(A,n,"");
 
     return 0;
 }
