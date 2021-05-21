4d7739614ce336622ab6de6ada3088500ce263c5
tamle2021 <tamle2021@zoho.com>
2021-05-21 14:56:22 -0500

    Update depth first search

--- c++/depth_first_search_stack.cpp
+++ c++/depth_first_search_stack.cpp
@@ -1,5 +1,11 @@
 /*
-Depth first search using stack
+Depth first search is a tree-based graph traversal algorithm that is used to search a graph. Unlike breadth first
+search, a DFS algorithm traverses a tree or graph from the parent vertex down to its children and
+grandchildren vertices in a single path until it reaches the end.  When there are no more vertices to visit
+in a path, the DFS algorithm will backtrack to a point where it can choose another path to take.  It will repeat
+the process again until all vertices have been visited.
+
+Depth first search using stack structure
 */
 
 #include <iostream>
@@ -7,11 +13,11 @@ Depth first search using stack
 
 using namespace std;
 
-void DFS(int u, int A[][8], int n) {
+void DFS(int u, int A[][8],int n) {
     // initialize visit tracking array and stack
     int visited[8] {0};
-    stack<int> stk;
-    stk.emplace(u);
+    stack<int> stac;
+    stac.emplace(u);
 
     // visit start vertex u
     cout << u << ", " << flush;
@@ -21,18 +27,18 @@ void DFS(int u, int A[][8], int n) {
     // initial adjacent vertex
     int v = 0;
 
-    while (!stk.empty()){
+    while (!stac.empty()){
         while (v < n){
             if (A[u][v] == 1 && visited[v] == 0){
                 // suspend exploring current vertex u
-                stk.push(u);
+                stac.push(u);
                 // ipdate current vertex as the next adjacent vertex
                 u = v;
 
                 // visit current vertex u
                 cout << u << ", " << flush;
                 visited[u] = 1;
-                // increment will make this 0
+                // increment will make 0
                 v = -1;
             }
             v++;
@@ -40,28 +46,28 @@ void DFS(int u, int A[][8], int n) {
         // can set v = 0 but setting v = u is better
         v = u;
         // return to previous suspended vertex
-        u = stk.top();
-        stk.pop();
+        u = stac.top();
+        stac.pop();
     }
 }
 
 // adds elements to stack from end
 void dfs(int u, int A[][8], int n){
     int visited[8] {0};
-    stack<int> stk;
-    stk.emplace(u);
+    stack<int> stac;
+    stac.emplace(u);
 
-    while (!stk.empty()){
-        u = stk.top();
-        stk.pop();
+    while (!stac.empty()){
+        u = stac.top();
+        stac.pop();
 
         if (visited[u] != 1){
             cout << u << ", " << flush;
             visited[u] = 1;
 
-            for (int v=n-1; v>=0; v--){
+            for (int v = n - 1; v >= 0; v--){
                 if (A[u][v] == 1 && visited[v] == 0){
-                    stk.emplace(v);
+                    stac.emplace(v);
                 }
             }
         }
@@ -80,13 +86,14 @@ int main (){
         {0, 0, 0, 0, 0, 1, 0, 0}};
 
     int u = 5;
-    cout << "depth first search vertex: " << u << " -> " << flush;
-    DFS(u, A, 8);
+    cout << "*****depth first search*****\n";
+    cout << "vertex: " << u << " => " << flush;
+    DFS(u,A,8);
     cout << endl;
 
     u = 3;
-    cout << "depth first search vertex: " << u << " -> " << flush;
-    dfs(u, A, 8);
+    cout << "vertex: " << u << " => " << flush;
+    dfs(u,A,8);
     cout << endl;
 
     return 0;
