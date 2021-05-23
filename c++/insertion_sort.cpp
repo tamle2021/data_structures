4bfc5bf654fca33e9ac74198bb441a263cbfdeb6
tamle2021 <tamle2021@zoho.com>
2021-05-23 14:22:55 -0500

    Update insertion sort

--- c++/insertion_sort.cpp
+++ c++/insertion_sort.cpp
@@ -1,6 +1,15 @@
 /*
+Insertion sort is a sorting algorithm that places an unsorted element at its suitable place in each iteration.  Insertion sort works similarly as we
+sort cards in our hand in a card game.  We assume that the first card is already sorted then, we select an unsorted card. If the unsorted card
+is greater than the card in hand, it is placed on the right otherwise, to the left. In the same way, other unsorted cards are taken and put in
+their right place.
+
 number of comparisons => (n * (n-1)) / 2 = O(n^2)
 number of swaps => O(n^2)
+best => O(n)
+worst => O(n^2)
+average => O(n^2)
+space complexity => O(1)
 adaptive & stable
 */
 #include <iostream>
@@ -35,11 +44,11 @@ int main() {
     int arr[] = {3,125,-99,133,19, 17, 15, 13, 11, 9, 7, 5, 3,-2,101,1,55};
     cout << "*****insertion sort*****" << endl;
     cout << "before sort: \n";
-    printArray(arr, sizeof(arr) / sizeof(arr[0]), "");
+    printArray(arr,sizeof(arr) / sizeof(arr[0]),"");
 
-    insertionSort(arr, sizeof(arr) / sizeof(arr[0]));
+    insertionSort(arr,sizeof(arr) / sizeof(arr[0]));
     cout << "after sort: \n";
-    printArray(arr, sizeof(arr) / sizeof(arr[0]), "");
+    printArray(arr,sizeof(arr) / sizeof(arr[0]),"");
 
     return 0;
 }
