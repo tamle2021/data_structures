0de4649c1ece057ac5bbd9f12563e99e8c1c344d
tamle2021 <tamle2021@zoho.com>
2021-06-01 06:49:11 -0500

    Add mountain array

---
+++ leet_valid_mountain_array.py
@@ -0,0 +1,29 @@
+'''
+Valid mountain array consists of increasing sequence followed by decreasing.
+
+
+'''
+from typing import List
+
+class ValidMountainArray:
+    def start(self,A: List[int]) -> bool:
+        if (len(A) < 3):
+            return False
+
+        i = 1
+        while (i < len(A) and A[i] > A[i - 1]):
+            i += 1
+
+        if (i == 1 or i == len(A)):
+            return False
+
+        while (i < len(A) and A[i] < A[i - 1]):
+            i += 1
+
+        return i == len(A)
+
+v = ValidMountainArray()
+result = v.start([1,1,2,3,1])
+print("result: {x}".format(x=result))
+
+
