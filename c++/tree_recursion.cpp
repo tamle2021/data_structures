e9c4f301274085853b885a317350c52609faff3c
tamle2021 <tamle2021@zoho.com>
2021-07-01 17:27:29 -0500

    Update tree recursion

--- c++/tree_recursion.cpp
+++ c++/tree_recursion.cpp
@@ -10,13 +10,13 @@ Consider one solution to the above problem:
 
 Breaking the procedure down, there are three parts to consider:
      1) There are two base cases, with two different outcomes.
-          * If there is only one step to climb, there is only one way (by taking that step)
-          * If there are two steps to climb, there is exactly two ways (1-step 1-step, or 2-step)
-     2) Otherwise, the problem is made smaller by breaking it into two worlds
-          * In the first world, we take one step, and thus the number of steps is reduced by one
-          * In the second world, we take two steps, and thus the number of steps is reduced by two
+          * If there is only one step to climb, there is only one way (by taking that step).
+          * If there are two steps to climb, there is exactly two ways (1-step 1-step, or 2-step).
+     2) Otherwise, the problem is made smaller by breaking it into two worlds.
+          * In the first world, we take one step, and thus the number of steps is reduced by one.
+          * In the second world, we take two steps, and thus the number of steps is reduced by two.
      3) Making two recursive calls to those smaller problems gives us the answer to those smaller problems, and adding up those
-         up gives us the answer to the original problem.
+         gives us the answer to the original problem.
 */
 
 #include <stdio.h>
