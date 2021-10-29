d36a8a5ae871cce29286d2a6091c4723d7922b06
tamle7422 <tamle7422@gmail.com>
2021-10-29 15:18:35 -0500

    update stack linked list

--- c++/stack_linked_list.cpp
+++ c++/stack_linked_list.cpp
@@ -1,50 +1,26 @@
 /*
 
+
 **** stack using linked list ****
 */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
 
 using namespace std;
 
-class Node{
-public:
+struct Node {
     int data;
-    Node* next;
-};
+    struct Node *next;
+}*top = NULL;
 
-class Stack{
-private:
-    Node* top;
-public:
-    Stack();
-    ~Stack();
-    void push(int x);
-    int pop();
-    int peek(int index);
-    int isEmpty();
-    int isFull();
-    int stackTop();
-};
+void push(int x) {
+    struct Node *t;
+    t = (struct Node*)malloc(sizeof(struct Node));
 
-Stack::Stack() {
-    top = nullptr;
-}
-
-Stack::~Stack() {
-    Node* p = top;
-    while (top) {
-        top = top->next;
-        delete p;
-        p = top;
-    }
-}
-
-void Stack::push(int x) {
-    Node* t = new Node;
-    if (t == nullptr) {
-        cout << "stack overflow...." << endl;
-    }
+    if (t == NULL)
+        printf("stack is full....\n");
     else {
         t->data = x;
         t->next = top;
@@ -52,85 +28,46 @@ void Stack::push(int x) {
     }
 }
 
-int Stack::pop() {
-    Node* p;
+int pop() {
+    struct Node *t;
     int x = -1;
-    if (top == nullptr) {
-        cout << "stack underflow: \nreturn element => ";
-    }
+
+    if (top == NULL)
+        printf("stack is empty\n");
     else {
-        p = top;
-        x = p->data;
+        t = top;
         top = top->next;
-        delete p;
+        x = t->data;
+        free(t);
     }
 
     return x;
 }
 
-int Stack::isFull() {
-    Node* t = new Node;
-    int r = t ? 1 : 0;
-    delete t;
+void displayIt() {
+    struct Node *p;
+    p = top;
 
-    return r;
-}
-
-int Stack::isEmpty() {
-    return top ? 0 : 1;
-}
-
-int Stack::stackTop() {
-    if (top) {
-        return top->data;
+    while (p != NULL) {
+        printf("%d ",p->data);
+        p = p->next;
     }
-    return -1;
-}
 
-int Stack::peek(int index) {
-    if (isEmpty()) {
-        return -1;
-    }
-    else {
-        Node* p = top;
-        for (int i = 0; p != nullptr && i < index - 1; i++) {
-            p = p->next;
-        }
-
-        if (p != nullptr) {
-            return p->data;
-        }
-        else {
-            return -1;
-        }
-    }
+    printf("\n");
 }
 
 int main() {
     cout << "**** stack using linked list ****\n";
-    int A[] = {11,34,15,3,0,9,44};
-    Stack stk;
+    // pushing values on top of stack
+    push(12);
+    push(29);
+    push(-5);
+    push(34);
+    push(101);
 
-    // populate stack
-    for (int i = 0; i < sizeof(A) / sizeof(A[0]); i++) {
-        stk.push(A[i]);
-    }
-
-    cout << "stack peek at 4: " << stk.peek(4) << endl;
-    cout << "stack peek at 9: " << stk.peek(9) << endl;
-    cout << "stack top: " << stk.stackTop() << endl;
-    cout << "is stack full? " << stk.isFull() << endl;
-    cout << "is stack empty? " << stk.isEmpty() << endl;
-
-    // pop out elements from stack
-    cout << "popped: " << flush;
-    for (int i = 0; i < sizeof(A) / sizeof(A[0]); i++){
-        cout << stk.pop() << " " << flush;
-    }
-
-    cout << endl;
-    // underflow
-    cout << stk.pop() << endl;
+    displayIt();
+    cout << "popping value off stack => " << pop();
+    //printf("%d ", pop());
 
     return 0;
 }
