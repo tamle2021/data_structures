553689c0a4b241692fa0c07d2df14ea5d14928cb
tamle7422 <tamle7422@gmail.com>
2021-11-16 10:46:52 -0600

    update contains duplicate

--- leetcode/contains_duplicate.py
+++ leetcode/contains_duplicate.py
@@ -1,5 +1,13 @@
 '''
-Checks for duplicate in array
+Given an integer array, return true if any value appears at least twice in array and return false if every element
+is distinct or no duplicate.
+
+example:
+input: nums = [1,2,3,1]
+output: true
+
+**** contains duplicate ****
+
 '''
 
 from collections import defaultdict
@@ -14,10 +22,18 @@ def containsDuplicate(nums: List[int]) -> bool:
         m[num] += 1
     return False
 
-arr = [4,0,1,2,6,99,87,4]
-result = containsDuplicate(arr)
+print("**** contains duplicate ****")
+arr1 = [4,0,1,2,6,99,87,4]
+arr2 = [25,3,-6,9,11,0,112,343]
+result1 = containsDuplicate(arr1)
+result2 = containsDuplicate(arr2)
 
-if (result):
-    print("contains duplicate...")
+if (result1):
+    print("array %s contains duplicate" % arr1)
 else:
-    print("no duplicate...")
+    print("array {0} contains no duplicate".format(arr1))
+
+if (result2):
+    print("array %s contains duplicate" % arr2)
+else:
+    print("array {0} contains no duplicate".format(arr2))
