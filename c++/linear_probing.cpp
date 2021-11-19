c8dbfc0c53880b9d754a3b167c09018cc40f7792
tamle7422 <tamle7422@gmail.com>
2021-11-19 15:25:05 -0600

    update linear probing

--- c++/linear_probing.cpp
+++ c++/linear_probing.cpp
@@ -1,9 +1,11 @@
 /*
-Linear probing is a scheme in computer programming for resolving collisions in hash tables and data structures for maintaining a collection of
-key-value pairs and looking up the value associated with a given key.  Along with quadratic probing and double hashing, linear probing is a
-form of open addressing. In these schemes, each cell of a hash table stores a single key-value pair. When the hash function causes a collision
-by mapping a new key to a cell of the hash table that is already occupied by another key, linear probing searches the table for the closest
-following free location and inserts the new key there.
+Linear probing is a scheme to resolve collisions in hash tables and data structures for maintaining a collection of key-value pairs
+and looking up value associated with given key.  Along with quadratic probing and double hashing, linear probing is a form
+of open addressing. In these schemes, each cell of a hash table stores a single key-value pair. When the hash function causes a
+collision by mapping a new key to a cell of hash table that is already occupied by another key, linear probing
+searches the table for closest following free location and inserts new key there.
+
+**** linear probing ****
 */
 
 #include <stdio.h>
@@ -26,7 +28,7 @@ int probe(int H[],int key) {
     return (index + i) % SIZE;
 }
 
-void Insert(int H[],int key) {
+void insertIt(int H[],int key) {
     int index = hashIt(key);
     if (H[index] != 0)
         index = probe(H,key);
@@ -34,27 +36,37 @@ void Insert(int H[],int key) {
     H[index] = key;
 }
 
-int Search(int H[],int key) {
+int searchIt(int H[],int key) {
     int index = hashIt(key);
     int i = 0;
 
-    while (H[(index + i) % SIZE] != key)
-        i++;
+    for (i = 0; i < sizeof(H) / sizeof(H[0]); i++) {
+        if (H[(index + i) % SIZE] == key)
+            return (index + i) % SIZE;
+    }
 
-    return (index + i) % SIZE;
+    // -1 for index not found
+    return -1;
 }
 
 int main() {
-    int HT[10] = {0};
-    Insert(HT,12);
-    Insert(HT,25);
-    Insert(HT,35);
-    Insert(HT,26);
-    Insert(HT,11);
-    Insert(HT,44);
+    int value1 = 44;
+    int value2 = 252;
+    int hashTable[15] = {0};
+    insertIt(hashTable,12);
+    insertIt(hashTable,25);
+    insertIt(hashTable,35);
+    insertIt(hashTable,26);
+    insertIt(hashTable,11);
+    insertIt(hashTable,44);
+    insertIt(hashTable,-7);
 
     cout << "**** linear probing ****\n";
-    printf("\nkey found at position: %d\n",Search(HT,44));
+    cout << "searching value: " << value1 << endl;
+    cout << "found at position => " << searchIt(hashTable,value1) << "\n";
+
+    cout << "searching value: " << value2 << endl;
+    cout << "found at position => " << searchIt(hashTable,value2) << "\n";
 
     return 0;
 }
