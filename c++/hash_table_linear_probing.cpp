f4b679c43fe545661db0f517e8392bc7a899ca08
tamle7422 <tamle7422@gmail.com>
2021-11-22 15:30:50 -0600

    update hash table linear probing

--- c++/hash_table_linear_probing.cpp
+++ c++/hash_table_linear_probing.cpp
@@ -1,9 +1,13 @@
 /*
-Linear probing only allows one item at each element.  There is no second dimension to look.  Linear probing is an example of open addressing.
-Open addressing collision resolution methods allow an item to put in a different spot other than what the hash function dictates.  Aside from linear
-probing, other open addressing methods include quadratic probing and double hashing.  With hash tables where collision resolution is handled via
-open addressing, each record actually has a set of hash indexes where they can go.  If the first location at the first hash index is occupied, it goes to
-the second, if that is occupied it goes to the third, etc.  The way this set of hash indexes is calculated depends on the probing method used.
+Linear probing only allows one item at each element.  There is no second dimension to look.  Linear probing is an example of open
+addressing.  Open addressing collision resolution methods allow an item to put in a different spot other than what the hash function
+dictates.  Aside from linear probing, other open addressing methods include quadratic probing and double hashing.  With hash tables
+where collision resolution is handled via open addressing, each record actually has a set of hash indexes where they can go.  If the
+first location at the first hash index is occupied, it goes to the second, if that is occupied it goes to the third, etc.  The way this set of
+hash indexes is calculated depends on the probing method used.
+
+**** linear probing in hash table ****
+
 */
 
 #include <iostream>
@@ -12,71 +16,73 @@ the second, if that is occupied it goes to the third, etc.  The way this set of 
 using namespace std;
 
 template <class T>
-void printIt(T& vec, int n, string s) {
+void printIt(T& vec,int n,string s) {
     cout << s << ": [" << flush;
-    for (int i=0; i<n; i++){
+    for (int i = 0; i < n; i++) {
         cout << vec[i] << flush;
-        if (i < n-1){
-            cout << ", " << flush;
+        if (i < n-1) {
+            cout << " " << flush;
         }
     }
 
     cout << "]" << endl;
 }
 
-int hashIt(int key){
+int hashIt(int key) {
     return key % SIZE;
 }
 
-int linearProbe(int H[], int key){
+int linearProbe(int H[], int key) {
     int idx = hashIt(key);
     int i = 0;
-    while (H[(idx + i) % SIZE] != 0){
+    while (H[(idx + i) % SIZE] != 0) {
         i++;
     }
     return (idx + i) % SIZE;
 }
 
-void insertIt(int H[],int key){
+void insertIt(int H[],int key) {
     int idx = hashIt(key);
-    if (H[idx] != 0){
+    if (H[idx] != 0) {
         idx = linearProbe(H,key);
     }
     H[idx] = key;
 }
 
-int searchIt(int H[],int key){
-    int idx = hashIt(key);
+int searchIt(int H[],int key) {
+    int index = hashIt(key);
     int i = 0;
-    while (H[(idx + i) % SIZE] != key) {
+    while (H[(index + i) % SIZE] != key) {
         i++;
-        if (H[(idx + i) % SIZE] == 0){
+        if (H[(index + i) % SIZE] == 0) {
             return -1;
         }
     }
-    return (idx + i) % SIZE;
+    return (index + i) % SIZE;
 }
 
 int main() {
-    int A[] = {26, 30, 45, 23, 25, 43, 74, 19, 29,35};
-    int n = sizeof(A) / sizeof(A[0]);
-
-    cout << "**** linear probing ****\n";
+    // questionable hashing logic
+    cout << "**** linear probing in hash table ****\n";
     printIt(A,n,"array");
+    int A[] = {19,7,40,1,44,23,25,43,74,0,12};
+    int n = sizeof(A) / sizeof(A[0]);
+    int value1 = 25;
+    int value2 = 35;
 
     // hash table
-    int HT[10] = {0};
-    for (int i=0; i<n; i++){
+    int HT[15] = {0};
+    for (int i = 0; i < n; i++) {
         insertIt(HT,A[i]);
     }
 
-    printIt(HT, SIZE, "hash table");
+    printIt(HT,SIZE,"hash table");
 
-    int index = searchIt(HT,25);
-    cout << "key found at: " << index << endl;
+    int index = searchIt(HT,value1);
+    cout << "searching " << value1 << " => key found at " << index << endl;
 
-    index = searchIt(HT,35);
-    cout << "key found at: " << index << endl;
+    index = searchIt(HT,value2);
+    cout << "searching " << value2 << " => key found at  " << index << endl;
 
     return 0;
 }
