1765a11288812bfc6cb7ebd505f0bc1c4dd8e7dc
tamle7422 <tamle7422@gmail.com>
2021-12-10 15:57:38 -0600

    update prim minimum spanning tree

--- c++/prim_minimum_spanning_tree.cpp
+++ c++/prim_minimum_spanning_tree.cpp
@@ -1,10 +1,13 @@
 /*
-A minimum spanning tree is a special kind of tree that minimizes the lengths or "weights" of tree edges.  An example is a cable
-company that wants to lay line to multiple neighborhoods; by minimizing the amount of cable laid, the cable company will save money.
-A tree has one path that joins any two vertices.
+A minimum spanning tree is a special kind of tree that minimizes the lengths or "weights" of tree edges.  A tree has one path that joins
+any two vertices.  An example is a cable company that wants to lay line in multiple neighborhoods.  By minimizing the amount of
+cable laid, the cable company will save money.
 
 A spanning tree of a graph is a tree that contains all the original graph’s vertices, reaches out to or spans all vertices, and is
 acyclic.  In other words, the graph does not have any nodes which loop back to itself.
+
+*** prim's minimum spanning tree ****
+
 */
 #include <iostream>
 #define V 8
@@ -12,90 +15,95 @@ acyclic.  In other words, the graph does not have any nodes which loop back to i
 
 using namespace std;
 
-void printMST(int T[][V-2], int G[V][V]){
-    cout << "Minimum spanning tree edges with cost:\n" << endl;
+void printMST(int T[][V-2],int G[V][V]) {
+    cout << "minimum spanning tree edges with cost: " << endl;
     int sum {0};
-    for (int i {0}; i < V - 2; i++){
+    for (int i {0}; i < V - 2; i++) {
         int c = G[T[0][i]][T[1][i]];
         cout << "[" << T[0][i] << "]---[" << T[1][i] << "] cost: " << c << endl;
         sum += c;
     }
     cout << endl;
-    cout << "Total cost of minimum spanning tree: " << sum << endl;
+    cout << "total cost of minimum spanning tree: " << sum << endl;
 }
 
-void primsMST(int G[V][V], int n){
+void primMinimumSpanT(int G[V][V],int n) {
     int u;
     int v;
     int min {I};
     int track [V];
     int T[2][V-2] {0};
 
-    // initial: find min cost edge
+    // find min cost edge
     for (int i {1}; i < V; i++) {
         // initialize track array with infinity
         track[i] = I;
-        for (int j {i}; j < V; j++){
-            if (G[i][j] < min){
+        for (int j {i}; j < V; j++) {
+            if (G[i][j] < min) {
                 min = G[i][j];
                 u = i;
                 v = j;
             }
         }
     }
+
     T[0][0] = u;
     T[1][0] = v;
     track[u] = track[v] = 0;
 
-    // initialize track array to track min cost edges
-    for (int i {1}; i < V; i++){
-        if (track[i] != 0){
-            if (G[i][u] < G[i][v]){
+    // initialize array to track min cost edges
+    for (int i {1}; i < V; i++) {
+        if (track[i] != 0) {
+            if (G[i][u] < G[i][v]) {
                 track[i] = u;
-            } else {
+            }
+            else {
                 track[i] = v;
             }
         }
     }
 
     // repeat
-    for (int i {1}; i < n - 1; i++){
+    for (int i {1}; i < n - 1; i++) {
         int k;
         min = I;
-        for (int j {1}; j < V; j++){
-            if (track[j] != 0 && G[j][track[j]] < min){
+        for (int j {1}; j < V; j++) {
+            if (track[j] != 0 && G[j][track[j]] < min) {
                 k = j;
                 min = G[j][track[j]];
             }
         }
+
         T[0][i] = k;
         T[1][i] = track[k];
         track[k] = 0;
 
-        // update track array to track min cost edges
-        for (int j {1}; j<V; j++){
-            if (track[j] != 0 && G[j][k] < G[j][track[j]]){
+        // update array to track min cost edges
+        for (int j {1}; j < V; j++) {
+            if (track[j] != 0 && G[j][k] < G[j][track[j]]) {
                 track[j] = k;
             }
         }
     }
-    printMST(T, G);
+    printMST(T,G);
 }
 
 int main() {
     int cost [V][V] {
-            {I, I, I, I, I, I, I, I},
-            {I, I, 25, I, I, I, 5, I},
-            {I, 25, I, 12, I, I, I, 10},
-            {I, I, 12, I, 8, I, I, I},
-            {I, I, I, 8, I, 16, I, 14},
-            {I, I, I, I, 16, I, 20, 18},
-            {I, 5, I, I, I, 20, I, I},
-            {I, I, 10, I, 14, 18, I, I},
+            {I,I,I,I,I,I,I,I},
+            {I,I,25,I,I,I,5,I},
+            {I,25,I,12,I,I,I,10},
+            {I,I,12,I,8,I,I,I},
+            {I,I,I,8,I,16,I,14},
+            {I,I,I,I,16,I,20,18},
+            {I,5,I,I,I,20,I,I},
+            {I,I,10,I,14,18,I,I},
     };
 
+    cout << "**** prim's minimum spanning tree ****\n";
     int n = sizeof(cost[0]) / sizeof(cost[0][0]) - 1;
 
-    primsMST(cost, n);
+    primMinimumSpanT(cost,n);
+
     return 0;
 }
