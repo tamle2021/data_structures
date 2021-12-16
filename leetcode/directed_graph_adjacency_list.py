a60f6c35673fe3d6c950d31ee65a600347fc8d5f
tamle7422 <tamle7422@gmail.com>
2021-12-16 11:37:55 -0600

    update directed graph

--- leetcode/directed_graph_adjacency_list.py
+++ leetcode/directed_graph_adjacency_list.py
@@ -1,7 +1,10 @@
 '''
-Adjacency list - directed graph implementation
-'''
+An adjacency list, also called an edge list, can be modeled as a representation of network.  Each edge in the network
+is indicated by listing the pair of nodes that are connected.  The order of nodes matter in directed graph.
 
+**** directed graph adjacency list  ****
+
+'''
 from collections import defaultdict
 
 class Graph:
@@ -14,11 +17,12 @@ class Graph:
     def printGraph(self):
         for node in self.graph:
             for v in self.graph[node]:
-                print(node, "->",v)
+                print(node,"->",v)
 
     def displayGraph(self):
         print(self.graph)
 
+print("**** directed graph adjacency list ****")
 g = Graph()
 g.insertEdge(1,2)
 g.insertEdge(2,3)
@@ -26,10 +30,7 @@ g.insertEdge(4,5)
 g.insertEdge(1,4)
 g.insertEdge(5,1)
 g.insertEdge(5,9)
-
-# 1 => [2]
-# 1 => [4]
-# 5 => [9]
+g.insertEdge(6,3)
 
 g.printGraph()
 g.displayGraph()
--- leetcode/directed_graph_adjacency_matrix.py
+++ leetcode/directed_graph_adjacency_matrix.py
@@ -1,7 +1,11 @@
 '''
-Adjacency matrix - directed graph implementation
-'''
+The adjacency matrix of a simple labeled graph is the matrix A with A [[i,j]] or 0 according to whether the
+vertex vj, is adjacent to the vertex vj or not. For simple graphs without self-loops, the adjacency matrix has zeros
+on the diagonal. For undirected graphs, the adjacency matrix is symmetric.
 
+**** directed graph adjacency matrix ****
+
+'''
 from collections import defaultdict
 
 class Graph:
@@ -14,26 +18,27 @@ class Graph:
         return (v1 >= 0 and v1 <= self.numberOfNodes) and (v2 >= 0 and v2 <= self.numberOfNodes)
 
     def insertEdge(self,v1,v2):
-        if(self.withInBounds(v1,v2)):
+        if (self.withInBounds(v1,v2)):
             self.graph[v1][v2] = 1
 
     def printGraph(self):
         for i in range(self.numberOfNodes):
             for j in range(len(self.graph[i])):
-                if(self.graph[i][j]):
+                if (self.graph[i][j]):
                     print(i, "->", j)
 
     def displayGraph(self):
         print("graph: {0}".format(self.graph))
 
-g = Graph(5)
+print("**** directed graph adjacency matrix ****")
+g = Graph(6)
 g.insertEdge(1,2)
 g.insertEdge(2,3)
 g.insertEdge(4,5)
 g.insertEdge(1,3)
 g.insertEdge(5,2)
-g.insertEdge(0,4)
-g.insertEdge(0,0)
+g.insertEdge(5,1)
+g.insertEdge(6,4)
 
 g.printGraph()
 g.displayGraph()
