37f7a0b95877a1aa5fdd164781949a99b87ebde8
tamle7422 <tamle7422@gmail.com>
2021-12-17 10:25:20 -0600

    update undirected graph adjacency list

--- leetcode/undirected_graph_adjacency_list.py
+++ leetcode/undirected_graph_adjacency_list.py
@@ -1,20 +1,22 @@
 '''
-Adjacency list - undirected graph implementation
-'''
+The main difference between directed and undirected graph is that a directed graph contains
+an ordered pair of vertices whereas an undirected graph contains an unordered pair of vertices.
 
+
+**** undirected graph adjacency list ****
+
+'''
 from collections import defaultdict
 
 class Graph:
     def __init__(self):
         self.graph = defaultdict(list)
-        # v1 => [v2,v3,v4]
 
     def insertEdge(self,v1,v2):
         self.graph[v1].append(v2)
         self.graph[v2].append(v1)
 
     def printGraph(self):
-        # loop over keys
         for node in self.graph:
             # loop over lists corresponding to key
             for v in self.graph[node]:
@@ -23,12 +25,15 @@ class Graph:
     def displayGraph(self):
         print("graph: %s" % self.graph)
 
+print("**** undirected graph adjacency list ****")
 g = Graph()
 g.insertEdge(1,5)
-g.insertEdge(1,100)
+g.insertEdge(5,1)
 g.insertEdge(1,2)
 g.insertEdge(2,3)
 g.insertEdge(4,5)
+g.insertEdge(5,4)
+g.insertEdge(3,6)
 
 g.printGraph()
 g.displayGraph()
