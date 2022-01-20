4890955481c8f663be3948510cb54529ac5a5e96
tamle7422 <tamle7422@gmail.com>
2022-01-20 10:43:46 -0600

    update hash table quadratic probing

--- c++/hash_table_quadratic_probing.cpp
+++ c++/hash_table_quadratic_probing.cpp
@@ -1,7 +1,11 @@
 /*
-A hash table is a data structure used to store key-value pairs. Hash function is used to compute an index into array, for which an element will be
-inserted or searched in hash table. Quadratic probing is a collision resolving technique in open addressed hash table. It operates by
-taking the original hash index and adding successive values of an arbitrary quadratic polynomial until an open slot is found.
+Hash table is a data structure used to store key-value pairs. Hash function is used to compute an index into array, for which an element
+will be inserted or searched in hash table. Quadratic probing is a collision resolving technique in open addressed hash table. It
+operates by taking hash index and adding successive values of an arbitrary quadratic polynomial until an open slot is found.
+
+
+**** hash table quadratic probing ****
+
 */
 #include <iostream>
 #define SIZE 10
@@ -20,11 +24,11 @@ void printArray(T& vec,int n,string s) {
     cout << "]" << endl;
 }
 
-int hashIt(int key){
+int hashIt(int key) {
     return key % SIZE;
 }
 
-int QuadraticProbe(int H[],int key) {
+int quadraticProbe(int H[],int key) {
     int idx = hashIt(key);
     int i = 0;
     while (H[(idx + i * i) % SIZE] != 0) {
@@ -33,21 +37,21 @@ int QuadraticProbe(int H[],int key) {
     return (idx + (i * i)) % SIZE;
 }
 
-void Insert(int H[],int key) {
+void insertIt(int H[],int key) {
     int idx = hashIt(key);
 
     if (H[idx] != 0) {
-        idx = QuadraticProbe(H,key);
+        idx = quadraticProbe(H,key);
     }
     H[idx] = key;
 }
 
-int searchKey(int H[],int key) {
+int searchIt(int H[],int key) {
     int idx = hashIt(key);
     int i = 0;
-    while (H[(idx + (i * i)) % SIZE] != key){
+    while (H[(idx + (i * i)) % SIZE] != key) {
         i++;
-        if (H[(idx + (i * i )) % SIZE] == 0){
+        if (H[(idx + (i * i )) % SIZE] == 0) {
             return -1;
         }
     }
@@ -55,25 +59,24 @@ int searchKey(int H[],int key) {
 }
 
 int main() {
-    cout << "**** hash table with quadratic probing ****" << endl;
-    int A[] = {0,-7,26,30,45,23,97,25,43,74,19,29,88};
+    // can't hash negative numbers correctly?
+    cout << "**** hash table quadratic probing ****" << endl;
+    int A[] = {11,22,30,45,3,25,43,74,18,44};
     int n = sizeof(A) / sizeof(A[0]);
     printArray(A,n,"array elements");
 
     // hash table
-    int HT[13] = {0};
+    int HT[10] = {0};
     for (int i = 0; i < n; i++) {
-        Insert(HT,A[i]);
+        insertIt(HT,A[i]);
     }
-    printArray(HT, SIZE,"hash table");
+    printArray(HT,SIZE,"hash table");
 
-    cout << "search element 88\n";
-    int index = searchKey(HT,88);
-    cout << "key found at: " << index << endl;
+    int index = searchIt(HT,30);
+    cout << "search element 30 --- key found at: " << index << endl;
 
-    cout << "search element 35\n";
-    index = searchKey(HT,35);
-    cout << "key found at: " << index << endl;
+    index = searchIt(HT,35);
+    cout << "search element 35 --- key found at: " << index << endl;
 
     return 0;
 }
