84d7cdd968eb33816de4f0aeebb9752066f5cd1e
tamle7422 <tamle7422@gmail.com>
2022-03-18 21:09:33 -0500

    add taylor series iterative

--- c++/taylor_series_iterative.cpp
+++ c++/taylor_series_iterative.cpp
@@ -1,23 +1,24 @@
 /*
-The taylor series formula is the representation of any function as an infinite sum of terms.   These terms are calculated from
-the values of the function’s derivatives at a single point. This concept is formulated by Scottish mathematician
-James Gregory, but formally introduced by English mathematician Brook Taylor in 1715.
+Taylor Series is an expansion of some function into an infinite sum of terms, where each term has a larger exponent like x, x2, x3,
+etc.
 
-**** taylor series iterative implementation ****
+
+**** taylor series iterative ****
+
 */
 #include <stdio.h>
 #include <iostream>
 
 using namespace std;
 
-double e(int x,int n) {
+double func1(int e,int x) {
     double s = 1;
     int i;
     double num = 1;
     double den = 1;
 
-    for (i = 1; i <= n; i++) {
-        num *= x;
+    for (i  = 1; i <= x; i++) {
+        num *= e;
         den *= i;
         s += num / den;
     }
@@ -26,7 +27,12 @@ double e(int x,int n) {
 }
 
 int main() {
-    cout << "**** taylor series iterative implementation ****\n";
-    printf("%lf \n",e(3,11));
+    cout << "**** taylor series iterative ****\n";
+    int e = 2;
+    int x = 8;
+
+    cout << e << " to the " << x << " = ";
+    printf("%lf \n",func1(e,x));
+
     return 0;
 }
