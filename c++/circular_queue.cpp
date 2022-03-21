6a4c0f782ccae88de15400ccbdd0a947f6799923
tamle7422 <tamle7422@gmail.com>
2022-03-21 11:46:45 -0500

    update circular queue

--- c++/circular_queue.cpp
+++ c++/circular_queue.cpp
@@ -1,114 +1,81 @@
 /*
-1) In case of a circular queue, head pointer will point to the front of queue and tail pointer will point to the end of queue.
-2) Initially, the head and the tail pointers will point to the same location and this would mean that queue is empty.
-3) New data is always added to location pointed by the tail pointer and once data is added, the tail pointer is incremented to point to
-next available location.
-4) In a circular queue, data is not actually removed from the queue. Only the head pointer is incremented by one position when dequeue is executed.
-As queue data is only data between the head and tail, hence data left outside is not a part of the queue anymore and removed.
-5) The head and tail pointer will get re-initialized to 0 every time they reach end of the queue.
-6) Also, the head and tail pointers can cross each other.  In other words, head pointer can be greater than tail.  This will happen
-when the queue gets dequeue a couple of times and tail pointer gets re-initialized upon reaching the end of the queue.
-*/
 
+
+**** circular queue ****
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
 
 using namespace std;
 
-class CircularQueue {
-private:
+struct Queue {
     int size;
     int front;
     int rear;
-    int* Q;
-public:
-    CircularQueue(int size);
-    ~CircularQueue();
-    bool isFull();
-    bool isEmpty();
-    void enqueue(int x);
-    int dequeue();
-    void display();
+    int *Q;
 };
 
-CircularQueue::CircularQueue(int size) {
-    this->size = size;
-    front = 0;
-    rear = 0;
-    Q = new int [size];
+void create(struct Queue *q,int size) {
+    q->size = size;
+    q->front = q->rear = 0;
+    q->Q = (int *)malloc(q->size * sizeof(int));
 }
 
-CircularQueue::~CircularQueue() {
-    delete [] Q;
-}
-
-bool CircularQueue::isEmpty() {
-    if (front == rear) {
-        return true;
-    }
-    return false;
-}
-
-bool CircularQueue::isFull() {
-    if ((rear + 1) % size == front) {
-        return true;
-    }
-    return false;
-}
-
-void CircularQueue::enqueue(int x) {
-    if (isFull()) {
-        cout << "queue overflow" << endl;
-    } else {
-        rear = (rear + 1) % size;
-        Q[rear] = x;
+void enqueue(struct Queue *q,int x) {
+    if ((q->rear + 1) % q->size == q->front)
+        printf("queue full....");
+    else {
+        q->rear = (q->rear + 1) % q->size;
+        q->Q[q->rear] = x;
     }
 }
 
-int CircularQueue::dequeue() {
+int dequeue(struct Queue *q) {
     int x = -1;
-    if (isEmpty()) {
-        cout << "queue underflow" << endl;
-    } else {
-        front = (front + 1) % size;
-        x = Q[front];
+
+    if (q->front == q->rear)
+        printf("queue empty....\n");
+    else {
+        q->front = (q->front + 1) % q->size;
+        x = q->Q[q->front];
     }
+
     return x;
 }
 
-void CircularQueue::display() {
-    int i = front + 1;
+void displayIt(struct Queue q) {
+    int i = q.front + 1;
+
     do {
-        cout << Q[i] << flush;
-        if (i < rear) {
-            cout << " <- " << flush;
-        }
-        i = (i + 1) % size;
-    } while (i != (rear + 1) % size);
+        printf("%d ",q.Q[i]);
+        i = (i + 1) % q.size;
+
+    } while (i != (q.rear + 1) % q.size);
+
+    printf("\n");
 }
 
 int main() {
-    int A[] = {4,11,19,57,81,167};
-    CircularQueue cq(sizeof(A) / sizeof(A[0]) + 1);
-
-    // enqueue
-    for (long long unsigned i = 0; i < sizeof(A) / sizeof(A[0]); i++) {
-        cq.enqueue(A[i]);
-    }
-
     cout << "**** circular queue ****\n";
-    // display
-    cq.display();
-    cout << endl;
-    // overflow
-    cq.enqueue(10);
+    struct Queue q;
 
-    // dequeue
-    for (long long unsigned i = 0; i < sizeof(A) / sizeof(A[0]); i++) {
-        cq.dequeue();
-    }
+    create(&q,8);
+    enqueue(&q,7);
+    enqueue(&q,14);
+    enqueue(&q,33);
+    enqueue(&q,25);
+    enqueue(&q,4);
+    enqueue(&q,92);
+    enqueue(&q,-5);
 
-    // underflow
-    cq.dequeue();
+    displayIt(q);
+    cout << "dequeue operation: ";
+    printf("%d \n",dequeue(&q));
+
+    cout << "display queue: ";
+    displayIt(q);
 
     return 0;
 }
