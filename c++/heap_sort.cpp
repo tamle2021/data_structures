236a7e43d71ef25fe7eb3e866258ce5597ae3e0b
tamle7422 <tamle7422@gmail.com>
2022-04-06 08:11:35 -0500

    update heap sort

--- c++/heap_sort.cpp
+++ c++/heap_sort.cpp
@@ -3,6 +3,7 @@ Min heap is a complete binary tree in which value in each internal node is small
 node.  If a node is stored at index k, then its left child is stored at index 2k + 1 and its right child at index 2k + 2.
 
 **** heap sort ****
+
 */
 #include <stdio.h>
 #include <iostream>
@@ -13,7 +14,8 @@ void insertIt(int A[],int n) {
     int i = n,temp;
     temp = A[i];
 
-    while(i > 1 && temp > A[i / 2]) {
+    // shifting
+    while (i > 1 && temp > A[i / 2]) {
         A[i] = A[i / 2];
         i = i / 2;
     }
@@ -22,7 +24,7 @@ void insertIt(int A[],int n) {
 }
 
 int deleteIt(int A[],int n) {
-    int i,j,x,temp,val;
+    int i,j,x,temp,val = 0;
     val = A[1];
     x = A[n];
     A[1] = A[n];
@@ -68,7 +70,7 @@ int main() {
     cout << "after heap: " << endl;
     for (i = 1; i <= 7; i++)
         printf("%d ",H[i]);
-
     printf("\n");
+
     return 0;
 }
