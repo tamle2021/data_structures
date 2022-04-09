0e278b09b3a3cfbd6b4886dee58d9d642bfbe702
tamle7422 <tamle7422@gmail.com>
2022-04-09 12:43:14 -0500

    update four sum

--- leetcode/four_sum_optimal.py
+++ leetcode/four_sum_optimal.py
@@ -1,6 +1,6 @@
 '''
 Given four integer arrays nums1, nums2, nums3, and nums4 all of length n, return the number of
-tuples (i, j, k, l) such that 0 <= i, j, k, l < n and nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+tuples (i,j,k,l) such that 0 <= i, j, k, l < n and nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
 
 example 1:
 input: nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
@@ -21,24 +21,36 @@ n == nums4.length
 time complexity: O(n^2)
 space complexity: O(n)
 
-*** non-optimal method ***
+*** optimal method ***
 **** four sum ****
 
 '''
 from typing import List
 
 class FourSum():
-    def begin(self,A: List[int],B: List[int],C: List[int], D: List[int]) -> int:
+    def begin(self,A: List[int],B: List[int],C: List[int],D: List[int]) -> int:
+        aMap = {}
         result = 0
 
         for i in range(0,len(A)):
+            x = A[i]
             for j in range(0,len(B)):
-                for k in range(0,len(C)):
-                    for l in range (0,len(D)):
-                        if (A[i] + B[j] + C[k] + D[l] == 0):
-                            result +=1
+                y = B[j]
+                if ((x + y) not in aMap):
+                    aMap[x + y] = 0
 
-        return result
+                aMap[x + y] += 1
+
+        for i in range(0,len(C)):
+            x = C[i]
+            for j in range(0,len(D)):
+                y = D[j]
+                target = -(x + y)
+
+                if (target in aMap):
+                    result += aMap[target]
+
+        return result,aMap
 
 num1 = [1,2]
 num2 = [-2,-1]
@@ -53,9 +65,11 @@ num8 = [-2,1]
 print("**** four sum ****")
 f = FourSum()
 print(f"arrays1: num1 = [1,2], num2 = [-2,-1], num3 = [-1,2], num4 = [0,2]")
-result1 = f.begin(num1,num2,num3,num4)
+result1,map1 = f.begin(num1,num2,num3,num4)
 print("result1: %s" % result1)
+print("map1: %s" % map1)
 
 print(f"arrays2: num5 = [0,2], num6 = [-1,3], num7 = [1,-1], num8 = [-2,1]")
-result2 = f.begin(num5,num6,num7,num8)
+result2,map2 = f.begin(num5,num6,num7,num8)
 print("result2: %s" % result2)
+print("map2: %s" % map2)
