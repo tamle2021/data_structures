c7030ad5d58d7c4be74805ab86bd787cc0117b87
tamle7422 <tamle7422@gmail.com>
2022-04-26 07:50:54 -0500

    update robot return to origin

--- leetcode/robot_return_to_origin.py
+++ leetcode/robot_return_to_origin.py
@@ -1,21 +1,35 @@
 '''
-There is a robot starting at the position (0,0), the origin, on a 2D plane. Given a sequence of its moves, judge
-if this robot ends up at (0, 0) after it completes its moves.
+There is a robot starting at position (0,0), the origin, on a 2D plane. Given a sequence of its moves,
+judge if this robot ends up at (0,0) after it completes moves.
 
-One is given a string moves that represents the move sequence of the robot where moves[i] represents its ith move.
-Valid moves are 'R' for right, 'L' for left, 'U' for up, and 'D' for down.  Return true if the robot returns to
-the origin after it finishes all of its moves, or false otherwise.
+One is given a string moves that represents the move sequence of robot where moves[i] represents its ith
+move.  Valid moves are 'R' for right, 'L' for left, 'U' for up, and 'D' for down.  Return true if robot
+returns to origin after it finishes all of its moves or false otherwise.
 
-The way that the robot is "facing" is irrelevant. 'R' will always make the robot move to the right once, 'L'
-will always make it move left, etc.  Also, assume that the magnitude of the robot's movement is the same
-for each move.
+The way that robot is "facing" is irrelevant. 'R' will always make robot move to right once and 'L'
+will make it move left.  Also, assume that magnitude of robot's movement is same for each move.
+
+# example 1
+input:
+moves = "UD"
+
+output:
+true
+
+explanation:
+The robot moves up once and then down once. All moves have same magnitude, so it ended up at origin
+where it started. Therefore, the answer is true.
 
 constraints:
-1 <= moves.length <= 2 * 104
-moves only contains the characters in the set {'U','D','L','R'}
-'''
+1 <= moves.length <= 2 * 10^4
+Moves only contain characters in set {'U','D','L','R'}.
 
-class RobotOrigin:
+
+**** robot return to origin ****
+
+
+'''
+class RobotReturnToOrigin():
     def judgeCircle(self,moves: str) -> bool:
         x = 0
         y = 0
@@ -29,13 +43,15 @@ class RobotOrigin:
                 y -= 1
             elif (move == 'L'):
                 x -= 1
+
         return x == 0 and y == 0
 
-r = RobotOrigin()
+print("**** robot return to origin ****")
+r = RobotReturnToOrigin()
 moves1 = "LDRRLRUULR"
 result1 = r.judgeCircle(moves1)
-print("return to origin? %s" % result1)
+print("moves1: %s\nresult1: %s" % (moves1,result1))
 
 moves2 = "LLLRRRUUUDDD"
 result2 = r.judgeCircle(moves2)
-print("return to origin? {0}".format(result2))
+print("moves2: {0}\nresult2: {1}".format(moves2,result2))
